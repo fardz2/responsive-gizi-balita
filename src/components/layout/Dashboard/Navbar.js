a3412045bc85cd36ea9f5c2fab059ecd2f269186
Muhammad Faridz <faridzmuhamad679@gmail.com>
2025-06-01 15:46:54 +0700

    update responsive pada admin

--- src/components/layout/Dashboard/Navbar.js
+++ src/components/layout/Dashboard/Navbar.js
@@ -1,43 +1,42 @@
 import { useRef, useState } from "react";
-import { FaBars } from 'react-icons/fa';
-import { IoMdNotificationsOutline } from 'react-icons/io';
-import { AiOutlineMail } from 'react-icons/ai';
+import { FaBars } from "react-icons/fa";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { AiOutlineMail } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import Logo from '../../../assets/img/GiziBalita_logo.png';
+import Logo from "../../../assets/img/GiziBalita_logo.png";
 
 const Navbar = ({ navbarLink }) => {
-
-    return (
-        <div
-            style={{ background: "#ffb4b4" }}
-            className={`flex mt-12 ml-20 justify-between max-h-[60px] w-full duration-300 pr-32 pl-8 py-4 shadow-sm z-10`}
-        >
-            <div>
-                <img src={Logo}
-                    alt="Image"
-                    style={{
-                        width: "100px",
-                        height: "auto",
-                        marginBottom: "10px",
-                    }} />
-            </div>
-            <div className="flex gap-4">
-                {
-                    navbarLink.map((link, index) => {
-                        return (
-                            <Link
-                                to={link.path}
-                                key={index}
-                                className="text-white hover:bg-rose-400 "
-                            >
-                                {link.name}
-                            </Link>
-                        );
-                    })
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div
+      style={{ background: "#ffb4b4" }}
+      className={`flex mt-12 lg:ml-20 justify-between max-h-[60px] w-full duration-300 pr-32 pl-8 py-4 shadow-sm z-10`}
+    >
+      <div>
+        <img
+          src={Logo}
+          alt="Image"
+          style={{
+            width: "100px",
+            height: "auto",
+            marginBottom: "10px",
+          }}
+        />
+      </div>
+      <div className="flex gap-4">
+        {navbarLink.map((link, index) => {
+          return (
+            <Link
+              to={link.path}
+              key={index}
+              className="text-white hover:bg-rose-400 "
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
